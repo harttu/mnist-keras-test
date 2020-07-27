19ac423673c917f2f834ce72d491f5b4ec6aa834
harttu <harttu.toivonen@gmail.com>
2020-07-27 18:18:24 +0300

    Create mnist-keras-test.py
    Eka toimiva

---
+++ mnist-keras-test.py
@@ -0,0 +1,35 @@
+print("Loading mnist data.")
+from keras.datasets import mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+print("Loading models and layers from keras.")
+from keras import models
+from keras import layers
+
+print("Stacking layers")
+network = models.Sequential()
+network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
+network.add(layers.Dense(10,activation='softmax'))
+
+print("Compiling network")
+network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+#import pdb; pdb.set_trace()
+
+print("Reshaping data")
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype('float32') / 255
+
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.astype('float32') / 255
+
+from keras.utils import to_categorical
+
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+network.fit(train_images, train_labels, epochs=5, batch_size=128)
+
+test_loss, test_acc = network.evaluate(test_images, test_labels)
+
+print('test_acc', test_acc)
